c3a8b96b473510634e163d5fdee16f727aed5929
csndylim <54385174+csndylim@users.noreply.github.com>
2020-04-14 01:15:45 +0800

    Added Doc2Vec, more GoogleMaps parameters, reviews

--- functions.py
+++ functions.py
@@ -1,9 +1,25 @@
+# =============================================================================
+# Import necessary libraries
+# =============================================================================
 from cleansing import *
 import pandas as pd
+
+#TF-IDF
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
+
+#Doc2Vec
+import nltk
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+from gensim.parsing.preprocessing import STOPWORDS
+from gensim.parsing.preprocessing import remove_stopwords
+from gensim.models.callbacks import CallbackAny2Vec
+
+#Visuals
 import gmaps # use google maps
 from ipywidgets.embed import embed_minimal_html #export google maps into html
 import calmap # plot the calendar
@@ -11,6 +27,9 @@ import matplotlib.pyplot as plt, mpld3 #export calendar into img
 
 def filterCalendar(user_startDate, user_endDate):
     filtercalDF =calendarDF[(calendarDF['date'] <= user_endDate) & (calendarDF['date'] >= user_startDate)]
+    
+    #  if num beds less than calendarDF.beds
+
     return filtercalDF
 
 def topListings(preference_textinput, algorithm_type,calendarDF):
@@ -19,48 +38,117 @@ def topListings(preference_textinput, algorithm_type,calendarDF):
     user_inputDF = pd.DataFrame(columns=['description'])
     user_inputDF['description'] = [preference_textinput]
     
-     # Collate the name, score, and other columns into on edataframe
-    def rec(index, aDF, scores):
-        count=0
-        recdf = pd.DataFrame(columns=['name', 'description','score','id','location','listing_url'])
-        for x in index:
-            recdf.at[count,'name']=aDF['name'][x]
-            recdf.at[count,'description']=aDF['description'][x]
-            recdf.at[count,'score']=scores[count]
-            recdf.at[count,'id'] = aDF['id'][x]
-            recdf.at[count,'location'] = aDF['location'][x]
-            recdf.at[count,'listing_url'] = aDF['listing_url'][x]
-            count+=1
-        return recdf
+    if(("TF-IDF" in algorithm_type) ==True):
+        
+        def rec(index, aDF, scores):
+            count=0
+            recdf = pd.DataFrame(columns=['name', 'description','score','id','location','listing_url','available'])
+            for x in index:
+                recdf.at[count,'name']=aDF['name'][x]
+                recdf.at[count,'description']=aDF['description'][x]
+                recdf.at[count,'score']=scores[count]
+                recdf.at[count,'id'] = aDF['id'][x]
+                recdf.at[count,'location'] = aDF['location'][x]
+                recdf.at[count,'listing_url'] = aDF['listing_url'][x]
+                recdf.at[count,'available'] = aDF['available'][x]
+                recdf.at[count,'reviews'] = aDF['reviews'][x]
+                count+=1
+            return recdf
+        
+        tfv = TfidfVectorizer()
+        desc = tfv.fit_transform((aDF['description'])) #fitting and transforming the vector 
+        user = tfv.transform(user_inputDF['description'])
+        aDF = aDF.reset_index(drop=True)
+        
+        if (algorithm_type == "Cosine-Similarity (TF-IDF)"):
+            cosScores = map(lambda x: cosine_similarity(user, x),desc)
+            wrap = list(cosScores)        
+            index = sorted(range(len(wrap)), key=lambda i: wrap[i], reverse=True)[:5] #Sort the index for top n recommendations
     
-    tfv = TfidfVectorizer()
-    desc = tfv.fit_transform((aDF['description'])) #fitting and transforming the vector 
-    user = tfv.transform(user_inputDF['description'])
-    aDF = aDF.reset_index(drop=True)
+            coslist=[]
+            for x in index:
+                coslist.append(wrap[x][0][0]) #Create a list of similarity scores
+            
+            results =rec(index,aDF,coslist)
+            return results
+                    
+        else: #K-Nearest Neighbour (TF-IDF)
+            n_neighbors = 5
+            
+            nei = NearestNeighbors(n_neighbors, p=2)
+            nei.fit(desc)
+            nn = nei.kneighbors(user, return_distance=True) 
+            
+            index = nn[1][0][0:] #//Change to start from index 0 to include the point itself
+            dist = nn[0][0][0:]
+            
+            results =rec(index,aDF,dist)
+            return results
+    else: # Cosine-Similarity (Doc2Vec)
+        
+        desclist = list(aDF['description']) #Convert the description into a list
+        
+        for x in range(len(desclist)):
+            desclist[x] = remove_stopwords(desclist[x]) #Remove stop words in description
     
-    if (algorithm_type == "Cosine-Similarity"):
-        cosScores = map(lambda x: cosine_similarity(user, x),desc)
-        wrap = list(cosScores)        
-        index = sorted(range(len(wrap)), key=lambda i: wrap[i], reverse=True)[:5] #Sort the index for top n recommendations
+        u_input = ' '.join(preference_textinput)
+        user_input_label = 'user_input'
+        
+        #Clean
+        cleaned_input = clean(u_input)
+        #Remove stop words
+        cleaned_removed_input = ' '.join([word for word in u_input.split() if word not in STOPWORDS])
+        #Tokenize
+        inputtoken = word_tokenize(cleaned_removed_input)
+        
+        desclist.append(cleaned_removed_input)
+        
+        #Create a copy of dataframe, Update the dataframe with the user input
+        bDF = aDF
+        bDF = bDF.append({'id':0, 'name':user_input_label, 'description':cleaned_removed_input, 'full_description':u_input}, ignore_index=True)
 
-        coslist=[]
-        for x in index:
-            coslist.append(wrap[x][0][0]) #Create a list of similarity scores
+        #Convert the listing id into string first, then into a list
+        bDF['id'] = bDF['id'].astype(str)
+        id_label = list(bDF['id'])
+        id_label.append('0')
         
-        results =rec(index,aDF,coslist)
-        return results
-                
-    else: # k nearest neighbour
-        n_neighbors = 5
+        #Tag each document
+        train_corpus = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[id_label[i]]) for i, _d in enumerate(desclist)]
         
-        nei = NearestNeighbors(n_neighbors, p=2)
-        nei.fit(desc)
-        nn = nei.kneighbors(user, return_distance=True) 
+        #Model training
+        epoch_logger = EpochLogger()
         
-        index = nn[1][0][0:] #//Change to start from index 0 to include the point itself
-        dist = nn[0][0][0:]
+        model = Doc2Vec(vector_size=80, min_count=2, epochs=20, dm=1, hs=1, window=2, compute_loss=True, callbacks=[epoch_logger])
+
+        #Build the vocab then train the model
+        model.build_vocab(train_corpus)
+
+        model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
         
-        results =rec(index,aDF,dist)
+        model.save("d2v.model")
+        print("Model Saved") #For visualization later
+        
+        model.docvecs['953595'] #This is where our vectors are stored in | Search by the unique tag
+        
+        #User input vector will be used to calculate similarity
+        vector = model.infer_vector(inputtoken)
+        
+        #Put the similarity in a list
+        for doc_id in range(len(train_corpus)):
+            inferred_vector = vector
+            print(doc_id)
+            sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+        
+        simsL = [list(e) for e in sims] #Convert to List of List
+        
+        #Put in dataframe
+        descDF = bDF[['name', 'description','id','location','listing_url','available','reviews']]
+        simsDF = pd.DataFrame(simsL, columns=['id','scores'])
+        results = pd.merge(descDF, simsDF, on='id')
+        results = results.sort_values(by=['scores'], ascending=False)
+        results = results.head(6) # get the top 5 IDs
+        results= results.iloc[1:] # remove first row
+        results = results.rename(columns={"scores": "score"})
         return results
     
 def googleMaps(dataset):
@@ -70,9 +158,11 @@ def googleMaps(dataset):
     info_box_template = """
     <dl>
     <dt>Name</dt><dd>{name}</dd>
-    <dt>id</dt><dd>{id}</dd>
-    <dt>score</dt><dd>{score}</dd>
-    <dt>location</dt><dd>{location}</dd>
+    <dt>ID</dt><dd>{id}</dd>
+    <dt>Score</dt><dd>{score}</dd>
+    <dt>Location</dt><dd>{location}</dd>
+    <dt>Availability (%)</dt><dd>{available}</dd>
+    <dt>URL</dt><dd>{listing_url}</dd>
     </dl>
     """
     dataset.drop(columns=['description'], inplace=True) # drop description as it is too long
@@ -99,3 +189,17 @@ def calendarImg(dataset):
         calmap.calendarplot(events, monthticks=1, daylabels='MTWTFSS',cmap='YlGn', fillcolor='grey', linewidth=1, fig_kws=dict(figsize=(16, 4)))
         name = "img"+str(i)+".png"
         plt.savefig(name)
+
+# Display start and end of epoch
+class EpochLogger(CallbackAny2Vec):
+     '''Callback to log information about training'''
+
+     def __init__(self):
+         self.epoch = 0
+
+     def on_epoch_begin(self, model):
+         print("Epoch #{} start".format(self.epoch))
+
+     def on_epoch_end(self, model):
+         print("Epoch #{} end".format(self.epoch))
+         self.epoch += 1
