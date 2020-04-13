94cf76456fde1ed46f2fc9edb96a2cc1735ff6db
csndylim <54385174+csndylim@users.noreply.github.com>
2020-04-14 01:16:45 +0800

    Added Doc2Vec, more GoogleMaps parameters, reviews

--- cleansing.py
+++ cleansing.py
@@ -4,15 +4,12 @@
 import pandas as pd 
 import re 
 import string
-from sklearn.feature_extraction import text 
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics.pairwise import linear_kernel
-from sklearn.feature_extraction.text import CountVectorizer
-from collections import Counter 
+from textblob import TextBlob
 
 try:
-   
-    # Import all the files
+    # =============================================================================
+    # Import the excel files
+    # =============================================================================
     listingsDF = pd.read_csv(r'C:\Users\Candy\OneDrive - Nanyang Technological University\Year 1 Semester 2\CZ1015\Mini Project\Git\listings.csv')
     
     calendarDF = pd.read_csv(r'C:\Users\Candy\OneDrive - Nanyang Technological University\Year 1 Semester 2\CZ1015\Mini Project\Git\calendar.csv')
@@ -22,8 +19,6 @@ try:
     # =============================================================================
     # Clean the data, doing pre-processing for usage
     # =============================================================================
-    #Clean listings
-
     listingsDF['location']= listingsDF[['latitude', 'longitude']].astype(str) .apply(lambda x: ','.join(x), axis=1) # combine latitute and longitude into one table
     
     listingsDF['location'] = ('(' + listingsDF['location']+')').astype(str) # add '(' to the front and ')' to the back
@@ -47,7 +42,6 @@ try:
     #Convert price from object to float
     listingsDF['price'] = listingsDF['price'].astype(float)
     
-    
     # =============================================================================
     # Extract the necessary columns to be used, into a dataframe
     # =============================================================================
@@ -55,34 +49,19 @@ try:
     aDF['full_description'] = aDF['description']
     
     # =============================================================================
-    # Clean the description column
+    # Clean any text column
     # =============================================================================
-    
     pd.set_option('display.max_colwidth', -1)
-    
+
     def clean(text):
         text = text.lower()
         text = re.sub('[^\w\s]', '', text) #This removes all the punctuations
         text = re.sub(r'\n',' ', text) #This relaces the \n with space
         text = re.sub(r'\r','', text) #\r
         text = re.sub('[^0-9a-z #+_]', '', text) #Special chars
-        
         return text
     
     aDF['description'] = aDF['description'].apply(clean)
-    
-    # =============================================================================
-    # Remove Stop words
-    # =============================================================================
-    
-    #Manually adding words
-    stop_words = text.ENGLISH_STOP_WORDS.union(["will"])
-    
-    #Use CountVectorizer to count the frequency of words in the descriptions
-    vec = CountVectorizer(stop_words=stop_words)
-    doc = vec.fit_transform(aDF['description'])
-    
-    aDF['words'] = aDF['description'].apply(lambda x: len(str(x).split())) #length of each description
     
     # =============================================================================
     # Include availability of each listings, to be displayed alongside the recommended listings
@@ -99,20 +78,40 @@ try:
     aDF = pd.merge(aDF, sortedDF, on="id")
     
     # =============================================================================
-    # Cosine similarity
+    # Get the polarity of the listingID 
     # =============================================================================
-    cos_simi = cosine_similarity(doc) #?? Ignores magnitude | s = cos(angle) = d1.d2 / ||d1|| * || d2|| where (d1.d2 = d1x*d2x + d1y*d2y)
-    
-    aDF['index'] = aDF.index
+    revDF = reviewsDF[['listing_id', 'comments']] 
+    revDF['comments'] = revDF['comments'].astype(str) #change to str so that can clean !!!
+    revDF['comments'] = revDF['comments'].apply(clean)
 
+    p = lambda x: TextBlob(x).sentiment.polarity
+    revDF['polarity'] = revDF['comments'].apply(p) 
+
+    revDF = revDF[['listing_id', 'polarity']]
+    revDF = revDF.groupby('listing_id').mean() #group by id and get the mean of polarity and subjectivity
+    revDF['id'] = revDF.index #id column became index so need to set it back as column
+    
+    # Get the reviews' sentiment
+    for index, row in revDF.iterrows():
+        if(revDF['polarity'][index]>0.5):
+            revDF.at[index, 'reviews'] = 'Mostly positive'
+        elif(revDF['polarity'][index]>0):
+            revDF.at[index, 'reviews'] = 'Positive'
+        elif(revDF['polarity'][index]<-0.5):
+            revDF.at[index, 'reviews'] = 'Mostly Negative'
+        elif(revDF['polarity'][index]<-0):
+            revDF.at[index, 'reviews'] = 'Negative'
+        else:
+            revDF.at[index, 'reviews'] = 'Neutral'
+    
+    aDF = pd.merge(aDF, revDF, on="id")
+    aDF['index'] = aDF.index
+    
     # =============================================================================
     # Keep listing_id that are found in both calendarDF and aDF - dont need to uncommment as both produce the same results
     # =============================================================================
-    
 #    aDF=  aDF.loc[((aDF.id.isin(calendarDF['listing_id']))),:]    
 #    calendarDF= calendarDF.loc[((calendarDF.listing_id.isin(aDF['id']))),:]    
-    
-    
     
 except IOError:
     print("File not found or file is opened. Please make sure your that file name is correct and it is placed in the correct directory. Remember to close it.")
